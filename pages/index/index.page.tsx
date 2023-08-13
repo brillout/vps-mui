aa6edea97de515e9d41552adb382e0a576084134
Romuald Brillout <git@brillout.com>
2023-08-13 10:50:27 +0200

    misc

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,14 +1,6 @@
 import { Counter } from './Counter'
 import Menu from '@mui/icons-material/Menu'
 import Button from '@mui/material/Button'
-// Workaround for https://github.com/mui/material-ui/issues/37375 (see https://github.com/brillout/vite-plugin-ssr/discussions/901 for more info).
-// Instead of having to do this every time we import a MUI component, we can:
-//  - Add MUI to ssr.noExternal for the production build: https://github.com/brillout/vite-plugin-ssr/discussions/901#discussioncomment-5985771
-//  - Use https://github.com/cyco130/vite-plugin-cjs-interop for automatically applying this workaround
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const MenuIcon: typeof Menu = Menu.default ?? Menu
-console.log('MenuIcon', MenuIcon)
 
 export { Page }
 
@@ -16,7 +8,6 @@ function Page() {
   return (
     <>
       <h1>Welcome</h1>
-      <MenuIcon />
       This page is:
       <ul>
         <li>Rendered to HTML.</li>
@@ -24,9 +15,12 @@ function Page() {
           Interactive. <Counter />
         </li>
       </ul>
-      <Button>
-        Navigate
-      </Button>
+      <div>
+        Menu: <Menu />
+      </div>
+      <div>
+        Button: <Button>Some button</Button>
+      </div>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
 import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 import { UserConfig } from 'vite'
+const isProd = process.env.NODE_ENV === 'production'
+
+// https://vitejs.dev/config/ssr-options.html#ssr-noexternal
+const noExternal: string[] = []
+if (isProd) {
+  noExternal.push(
+    ...[
+      // prettier-ignore
+      // MUI needs to be pre-processed by Vite in production: https://github.com/brillout/vite-plugin-ssr/discussions/901
+      '@mui/base',
+      '@mui/icons-material',
+      '@mui/material',
+      '@mui/utils'
+    ]
+  )
+}
 
 const config: UserConfig = {
   plugins: [react(), ssr()],
   ssr: {
-    noExternal: ['@mui/material']
+    noExternal
   }
 }
 
