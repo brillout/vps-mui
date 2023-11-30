fb91629ebbcf6d9ca0bdf5fbe28c815ace5f27f3
Mike Wyatt <wyatt.mike@gmail.com>
2023-11-30 13:05:36 -0400

    use CacheProvider in PageShell

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -4,26 +4,29 @@ import { PageContextProvider } from './usePageContext'
 import type { PageContext } from './types'
 import './PageShell.css'
 import { Link } from './Link'
+import { CacheProvider, type EmotionCache } from '@emotion/react'
 
 export { PageShell }
 
-function PageShell({ children, pageContext }: { children: React.ReactNode; pageContext: PageContext }) {
+function PageShell({ children, pageContext, emotionCache }: { children: React.ReactNode; pageContext: PageContext, emotionCache: EmotionCache }) {
   return (
     <React.StrictMode>
-      <PageContextProvider pageContext={pageContext}>
-        <Layout>
-          <Sidebar>
-            <Logo />
-            <Link className="navitem" href="/">
-              Home
-            </Link>
-            <Link className="navitem" href="/about">
-              About
-            </Link>
-          </Sidebar>
-          <Content>{children}</Content>
-        </Layout>
-      </PageContextProvider>
+      <CacheProvider value={emotionCache}>
+        <PageContextProvider pageContext={pageContext}>
+          <Layout>
+            <Sidebar>
+              <Logo />
+              <Link className="navitem" href="/">
+                Home
+              </Link>
+              <Link className="navitem" href="/about">
+                About
+              </Link>
+            </Sidebar>
+            <Content>{children}</Content>
+          </Layout>
+        </PageContextProvider>
+      </CacheProvider>
     </React.StrictMode>
   )
 }
